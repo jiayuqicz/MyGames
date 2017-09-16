30ff8ef5924545a0745475116eb4f9d40dd6ea9c
jiayuqicz <jiayuqicz@gmail.com>
2017-09-16 19:42:46 +0800

    设置全屏

--- android/src/com/jiayuqicz/game/AndroidLauncher.java
+++ android/src/com/jiayuqicz/game/AndroidLauncher.java
@@ -1,6 +1,8 @@
 package com.jiayuqicz.game;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
@@ -9,9 +11,24 @@ public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+        fullScreen();
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		config.useCompass = false;
         config.useAccelerometer = false;
 		initialize(new DropGame(), config);
 	}
+
+	//设置全屏
+	public void fullScreen() {
+        if(Build.VERSION.SDK_INT >14 && Build.VERSION.SDK_INT < 19) {
+            View view = this.getWindow().getDecorView();
+            view.setSystemUiVisibility(View.GONE);
+        }
+        else if(Build.VERSION.SDK_INT >=19) {
+            View view = getWindow().getDecorView();
+            int uiOption = View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+            view.setSystemUiVisibility(uiOption);
+        }
+    }
 }
